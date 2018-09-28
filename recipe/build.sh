0e6eb1e634b99a63060870b77b40c284e669b925
Ray Donnelly <mingw.android@gmail.com>
2018-09-28 08:14:54 +0100

    Force install of xsv (it was already installed on macOS)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
 
-./install.sh --prefix=$PREFIX
+echo "$CC $@" > cc
+echo "#!/usr/bin/env bash"   > ./cc
+echo "${CC} \"\$@\""        >> ./cc
+chmod +x cc
+
+PATH="${PWD}:$PATH" ./install.sh --prefix=${PREFIX}
