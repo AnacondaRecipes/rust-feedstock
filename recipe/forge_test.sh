b48ba422414dcdf663da1d499f18d91703f041a0
Steven <scrass@anaconda.com>
2024-03-12 13:43:00 -0600

    clean up tests

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -e -x
 
-## TODO: remove the following `unset` lines, once the following issue in `conda-build` is resolved:
-##       <https://github.com/conda/conda-build/issues/2255>
-
-# unset REQUESTS_CA_BUNDLE
-# unset SSL_CERT_FILE
-
 rustc --version
 rustc --help
 rustdoc --help
