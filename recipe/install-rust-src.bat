1fbc3954ce0477b12439d7129e8bf090cf8b4d73
Jack Olivieri <jolivieri@anaconda.com>
2024-04-09 00:10:00 +0200

    remove unnecessary build scripts

--- recipe/install-rust-src.bat
+++
@@ -1,9 +0,0 @@
-pushd rust-src
-
-echo %target_platform%
-
-echo %PKG_NAME% > ./components
-
-FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "pfx=%%i"
-bash -lce "%SRC_DIR%/install-rust-src.sh" --prefix=%pfx%/Library
-if errorlevel 1 exit 1
--- recipe/install-rust-std.bat
+++
@@ -1,9 +0,0 @@
-pushd rust-std
-
-echo %target_platform%
-
-echo %PKG_NAME% > ./components
-
-FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "pfx=%%i"
-bash -lce "%SRC_DIR%/install.sh" --prefix=%pfx%/Library
-if errorlevel 1 exit 1
