b7bace0d558a837afb8413eee14f1c41805738ef
Jack Olivieri <jolivieri@anaconda.com>
2024-04-08 20:52:47 +0200

    refine build scripts

--- recipe/install-rust-std.bat
+++ recipe/install-rust-std.bat
@@ -1,4 +1,9 @@
 pushd rust-std
+
+echo %target_platform%
+
+echo %PKG_NAME% > ./components
+
 FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "pfx=%%i"
-bash -lce "%SRC_DIR%/install-rust-std.sh" --prefix=%pfx%/Library
+bash -lce "%SRC_DIR%/install.sh" --prefix=%pfx%/Library
 if errorlevel 1 exit 1
--- recipe/install-rust-std.sh
+++ recipe/install-rust-std.sh
@@ -2,13 +2,10 @@
 
 set -ex
 
-cd $SRC_DIR/rust-std
+echo ${target_platform}
 
 echo $PKG_NAME > ./components
 
-PATH="${PWD}:$PATH" ./install.sh --prefix="${PREFIX}"
+$SRC_DIR/rust-std/install.sh --prefix="${PREFIX}"
 
-rm "${PREFIX}"/lib/rustlib/components
 rm "${PREFIX}"/lib/rustlib/install.log
-rm "${PREFIX}"/lib/rustlib/rust-installer-version
-rm "${PREFIX}"/lib/rustlib/uninstall.sh
