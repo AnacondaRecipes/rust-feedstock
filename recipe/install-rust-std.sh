095c88a557199e01cc018eeb8e944142b584ea5f
Jack Olivieri <jolivieri@anaconda.com>
2025-03-07 18:33:09 +0100

    fix install scripts

---
+++ recipe/install-rust-std.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cd rust-std && ./install.sh --prefix="${PREFIX}"
+
+rm $PREFIX/lib/rustlib/manifest-rust-std-${rust_arch}
+rm $PREFIX/lib/rustlib/rust-installer-version
+rm $PREFIX/lib/rustlib/install.log
+rm $PREFIX/lib/rustlib/components
+rm $PREFIX/lib/rustlib/uninstall.sh
