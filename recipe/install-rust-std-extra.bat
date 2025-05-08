976bd3fa56418c6bcd6287585be9f46ff411864b
Andrii Osipov <aosipov@anaconda.com>
2025-05-08 06:15:58 -0700

    rewrite extra.bat

--- recipe/install-rust-std-extra.bat
+++ recipe/install-rust-std-extra.bat
@@ -1,6 +1,10 @@
 @echo off
 
-cd rust-std-%rust_std_extra% && call install.bat --prefix="%PREFIX%\Library"
+cd rust-std-%rust_std_extra%
+
+FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "pfx=%%i"
+bash install.sh --prefix=%pfx%/Library
+if errorlevel 1 exit 1
 
 del /f /q %PREFIX%\Library\lib\rustlib\manifest-rust-std-%rust_std_extra%
 del /f /q %PREFIX%\Library\lib\rustlib\rust-installer-version
