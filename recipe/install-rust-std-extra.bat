ac480493ec7f8f3cf606df4edfb081400b9ca0f2
Andrii Osipov <aosipov@anaconda.com>
2025-05-08 00:38:10 -0700

    fix typo

--- recipe/install-rust-std-extra.bat
+++ recipe/install-rust-std-extra.bat
@@ -1,9 +1,9 @@
 @echo off
 
-cd rust-std-%rust_std_extra% && call install.bat --prefix="%PREFIX%"
+cd rust-std-%rust_std_extra% && call install.bat --prefix="%PREFIX%\Library"
 
-del /f /q %PREFIX%\lib\rustlib\manifest-rust-std-%rust_std_extra%
-del /f /q %PREFIX%\lib\rustlib\rust-installer-version
-del /f /q %PREFIX%\lib\rustlib\install.log
-del /f /q %PREFIX%\lib\rustlib\components
-del /f /q %PREFIX%\lib\rustlib\uninstall.sh
+del /f /q %PREFIX%\Library\lib\rustlib\manifest-rust-std-%rust_std_extra%
+del /f /q %PREFIX%\Library\lib\rustlib\rust-installer-version
+del /f /q %PREFIX%\Library\lib\rustlib\install.log
+del /f /q %PREFIX%\Library\lib\rustlib\components
+del /f /q %PREFIX%\Library\lib\rustlib\uninstall.sh
