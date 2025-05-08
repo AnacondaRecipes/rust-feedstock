4c4222a6dde1383107fae6cb82aec5d397632e4b
Andrii Osipov <aosipov@anaconda.com>
2025-05-08 00:07:38 -0700

    remove unused script

--- recipe/install-rust-std.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-cd rust-std && ./install.sh --prefix="${PREFIX}"
-
-rm -f $PREFIX/lib/rustlib/manifest-rust-std-${rust_arch}
-rm -f $PREFIX/lib/rustlib/rust-installer-version
-rm -f $PREFIX/lib/rustlib/install.log
-rm -f $PREFIX/lib/rustlib/components
-rm -f $PREFIX/lib/rustlib/uninstall.sh
