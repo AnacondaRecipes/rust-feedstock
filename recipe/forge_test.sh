cf276b78855ab601015750a093e51186e7364d63
Ivan Iziumov <iiziumov@anaconda.com>
2025-10-09 12:18:24 +0200

    rust-nightly 1.89.0

--- recipe/forge_test.sh
+++ recipe/forge_test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e -x
 
 echo "#!/usr/bin/env bash"                         > ./cc
-if [[ ${target_platform} =~ linux.*390x.* ]]; then
-   echo "s390x-conda_cos7-linux-gnu-cc \"\$@\""   >> ./cc
-elif [[ ${target_platform} =~ linux.*aarch64* ]]; then
+if [[ ${target_platform} =~ linux.*aarch64* ]]; then
   echo "aarch64-conda-linux-gnu-cc \"\$@\""   >> ./cc
 elif [[ ${target_platform} =~ linux.* ]]; then
   echo "x86_64-conda_cos7-linux-gnu-cc \"\$@\""   >> ./cc
@@ -14,4 +12,5 @@ cat cc
 chmod +x cc
 
 mkdir ~/tmp-cargo || true
-CARGO_TARGET_DIR=~/tmp-cargo PATH="$PWD:$PATH" cargo install xsv --force -vv
+CARGO_TARGET_DIR=~/tmp-cargo
+cargo install xsv --force -vv
